8759791c225bb7464eb2734a55d3cb87e7e929ca
Rajab Natshah <rajabn@gmail.com>
2018-05-29 04:28:46 +0300

    Issue #2975920: Added [Varbase Video Player] library, along with varbase_media/youtube_player and varbase_media/vimeo_player libraries to have play External and locally hosted videos

---
+++ js/varbase-video-player.js
@@ -0,0 +1,39 @@
+/**
+ * @file
+ * Behaviors Varbase Video Player general scripts.
+ */
+
+(function ($, _, Drupal, drupalSettings) {
+  "use strict";
+
+    Drupal.behaviors.varbaseVideoPlayer = {
+      attach: function (context, settings) {
+
+      $('.js-video-player-icon').on("click", function(ev) {
+        $(this).fadeOut(500);
+
+        // Locally hosted Video.
+        if ($(this).closest(".field--name-field-video").find('.media--type-video video').length > 0) {
+          $(this).closest(".field--name-field-video").find('.media--type-video video').get(0).play();
+        }
+
+        // Youtube.
+        if ($(this).closest('.field--name-field-video').find('.media--type-video-embed iframe[src*="youtube.com"]').length > 0) {
+          var closestYoutubeIframe = $(this).closest('.field--name-field-video').find('.media--type-video-embed iframe[src*="youtube.com"]');
+          var youtubeURL = String(closestYoutubeIframe.get(0).src); 
+          youtubeURL = youtubeURL.replace(/autoplay=0/gi, "autoplay=1");
+          closestYoutubeIframe.get(0).src = youtubeURL;
+          ev.preventDefault();
+        }
+
+        // Vimeo.
+        if ($(this).closest('.field--name-field-video').find('iframe[src*="vimeo.com"]').length > 0) {
+          var closestVimoIframe = $(this).closest('.field--name-field-video').find('iframe[src*="vimeo.com"]').get(0);
+          var vimoPlayer = new Vimeo.Player(closestVimoIframe);
+          vimoPlayer.play();
+        }
+
+      });
+    }
+  };
+})(window.jQuery, window._, window.Drupal, window.drupalSettings);
