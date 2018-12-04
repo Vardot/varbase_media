73de3911b29c97464899316a3157ef4b9b6cdfd1
ahmadhalah <ahmadhalah@3354734.no-reply.drupal.org>
2018-12-04 17:01:43 +0200

    Issue #3016407 by ahmadhalah: YouTube does not play directly after clicking on thumbnail

--- js/varbase-video-player.js
+++ js/varbase-video-player.js
@@ -6,10 +6,10 @@
 (function ($, _, Drupal, drupalSettings) {
   "use strict";
 
-    Drupal.behaviors.varbaseVideoPlayer = {
-      attach: function (context, settings) {
+  Drupal.behaviors.varbaseVideoPlayer = {
+    attach: function (context, settings) {
 
-        $('.js-video-player-icon').on("click", function(ev) {
+      $('.js-video-player-icon').on("click", function (ev) {
         $(this).fadeOut(500);
 
         // Locally hosted Video.
@@ -20,13 +20,19 @@
         // Youtube.
         if ($(this).closest('.field.field--type-entity-reference').find('.media--type-video-embed iframe[src*="youtube.com"]').length > 0) {
           var closestYoutubeIframe = $(this).closest('.field.field--type-entity-reference').find('.media--type-video-embed iframe[src*="youtube.com"]');
-          var youtubeURL = String(closestYoutubeIframe.get(0).src); 
+          var youtubeURL = String(closestYoutubeIframe.get(0).src);
           youtubeURL = youtubeURL.replace(/autoplay=0/gi, "autoplay=1");
           youtubeURL = youtubeURL.replace(/controls=0/gi, "controls=1");
+          youtubeURL = youtubeURL + "&enablejsapi=1";
           closestYoutubeIframe.get(0).src = youtubeURL;
+
+          // Youtube API.
+          var yotubePlayer = new YT.Player(closestYoutubeIframe.get(0));
+          yotubePlayer.playVideo();
+
           ev.preventDefault();
         }
-        
+
         // Vimeo.
         if ($(this).closest('.field.field--type-entity-reference').find('iframe[src*="vimeo.com"]').length > 0) {
           var closestVimoIframe = $(this).closest('.field.field--type-entity-reference').find('iframe[src*="vimeo.com"]').get(0);
