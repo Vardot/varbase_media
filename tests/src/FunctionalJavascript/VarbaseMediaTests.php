6ddd08935de4ff8db6eca97127b9dd7318e31dea
Rajab Natshah <rajabn@gmail.com>
2021-09-13 10:59:15 +0300

    Issue #3232617: Switch Varbase Media to use Entity Definition Update Manager class from vardot/entity-definition-update-manager class library

---
+++ tests/src/FunctionalJavascript/VarbaseMediaTests.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Drupal\Tests\varbase_media\FunctionalJavascript;
+
+use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Varbase Media tests.
+ *
+ * @group varbase_media
+ */
+class VarbaseMediaTests extends WebDriverTestBase {
+
+  use StringTranslationTrait;
+
+  /**
+   * Modules to install.
+   *
+   * @var array
+   */
+  public static $modules = [
+    'metatag',
+    'metatag_views',
+    'better_exposed_filters',
+    'varbase_media',
+    'varbase_media_instagram',
+    'varbase_media_twitter',
+  ];
+
+  /**
+   * Specify the theme to be used in testing.
+   *
+   * @var string
+   */
+  protected $defaultTheme = 'vartheme_bs4';
+
+  /**
+   * The profile to install as a basis for testing.
+   *
+   * @var string
+   */
+  protected $profile = 'standard';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    // Insall the Claro admin theme.
+    $this->container->get('theme_installer')->install(['vartheme_claro']);
+
+    // Set the Claro theme as the default admin theme.
+    $this->config('system.theme')->set('admin', 'vartheme_claro')->save();
+
+  }
+
+  /**
+   * Check Varbase Media Types.
+   */
+  public function testCheckVarbaseMediaTypesPage() {
+
+    // Given that the root super user was logged in to the site.
+    $this->drupalLogin($this->rootUser);
+
+    $this->drupalGet('/admin/structure/media');
+
+    $this->assertSession()->pageTextContains($this->t('Image'));
+    $this->assertSession()->pageTextContains($this->t('Remote video'));
+    $this->assertSession()->pageTextContains($this->t('Video'));
+    $this->assertSession()->pageTextContains($this->t('Gallery'));
+    $this->assertSession()->pageTextContains($this->t('Instagram'));
+    $this->assertSession()->pageTextContains($this->t('Tweet'));
+    $this->assertSession()->pageTextContains($this->t('Document'));
+
+  }
+
+  /**
+   * Check Varbase Media settings page.
+   */
+  public function testCheckVarbaseMediaSettings() {
+
+    // Given that the root super user was logged in to the site.
+    $this->drupalLogin($this->rootUser);
+
+    $this->drupalGet('/admin/people/permissions#module-varbase_media');
+    $this->assertSession()->pageTextContains($this->t('Administer Varbase Media settings'));
+  }
+
+  /**
+   * Check permissions to Administer Varbase Media settings.
+   */
+  public function testCheckVarbaseMediaSettingsPermissions() {
+
+    // Given that the root super user was logged in to the site.
+    $this->drupalLogin($this->rootUser);
+
+    $this->drupalGet('/admin/people/permissions');
+    $this->assertSession()->pageTextContains($this->t('Administer Varbase Media settings'));
+  }
+
+}
