42759d8936cef1370a55c1842f785a213242a20a
Rajab Natshah <rajabn@gmail.com>
2024-01-07 17:54:55 +0300

    Issue #3402234: Fix not showing inline images in Node edit form ( CKEditor 5 ) when inserted using the media library button

---
+++ js/varbase-media-ckeditor-drimage.js
@@ -0,0 +1,57 @@
+/**
+ * @file
+ * Provides Varbase Media CKEditor 5 integration with Drimage.
+ */
+
+((Drupal) => {
+  Drupal.behaviors.varbaseMediaCKEditorDrimage = {
+    attach: function (context) {
+
+      var timer;
+      clearTimeout(timer);
+      timer = setTimeout(Drupal.drimage.init, 5, document);
+
+      addEventListener('mouseenter', function () {
+        clearTimeout(timer);
+        timer = setTimeout(Drupal.drimage.init, 100);
+      });
+
+      addEventListener('focus', function () {
+        clearTimeout(timer);
+        timer = setTimeout(Drupal.drimage.init, 100);
+      });
+
+      addEventListener('click', function () {
+        clearTimeout(timer);
+        timer = setTimeout(Drupal.drimage.init, 100);
+      });
+
+      addEventListener('mouseleave', function () {
+        clearTimeout(timer);
+        timer = setTimeout(Drupal.drimage.init, 100);
+      });
+
+      addEventListener('mouseover', function () {
+        clearTimeout(timer);
+        timer = setTimeout(Drupal.drimage.init, 100);
+      });
+
+      addEventListener('blur', function () {
+        clearTimeout(timer);
+        timer = setTimeout(Drupal.drimage.init, 100);
+      });
+
+      addEventListener('keyup', function () {
+        clearTimeout(timer);
+        timer = setTimeout(Drupal.drimage.init, 100);
+      });
+
+      addEventListener('toggle', function () {
+        clearTimeout(timer);
+        timer = setTimeout(Drupal.drimage.init, 100);
+      });
+
+    }
+  };
+
+})(Drupal);
