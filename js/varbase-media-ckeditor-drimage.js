69f2d7d8cd00decd31899ad66501206756f9471a
Rajab Natshah <33889-RajabNatshah@users.noreply.drupalcode.org>
2024-06-04 15:26:54 +0000

    Issue #3451094: Switch from Drimage to Drimage Improved in Varbase Media

--- js/varbase-media-ckeditor-drimage.js
+++ js/varbase-media-ckeditor-drimage.js
@@ -9,46 +9,46 @@
 
       var timer;
       clearTimeout(timer);
-      timer = setTimeout(Drupal.drimage.init, 5, document);
+      timer = setTimeout(Drupal.drimage_improved.init, 5, document);
 
       addEventListener('mouseenter', function () {
         clearTimeout(timer);
-        timer = setTimeout(Drupal.drimage.init, 100);
+        timer = setTimeout(Drupal.drimage_improved.init, 100);
       });
 
       addEventListener('focus', function () {
         clearTimeout(timer);
-        timer = setTimeout(Drupal.drimage.init, 100);
+        timer = setTimeout(Drupal.drimage_improved.init, 100);
       });
 
       addEventListener('click', function () {
         clearTimeout(timer);
-        timer = setTimeout(Drupal.drimage.init, 100);
+        timer = setTimeout(Drupal.drimage_improved.init, 100);
       });
 
       addEventListener('mouseleave', function () {
         clearTimeout(timer);
-        timer = setTimeout(Drupal.drimage.init, 100);
+        timer = setTimeout(Drupal.drimage_improved.init, 100);
       });
 
       addEventListener('mouseover', function () {
         clearTimeout(timer);
-        timer = setTimeout(Drupal.drimage.init, 100);
+        timer = setTimeout(Drupal.drimage_improved.init, 100);
       });
 
       addEventListener('blur', function () {
         clearTimeout(timer);
-        timer = setTimeout(Drupal.drimage.init, 100);
+        timer = setTimeout(Drupal.drimage_improved.init, 100);
       });
 
       addEventListener('keyup', function () {
         clearTimeout(timer);
-        timer = setTimeout(Drupal.drimage.init, 100);
+        timer = setTimeout(Drupal.drimage_improved.init, 100);
       });
 
       addEventListener('toggle', function () {
         clearTimeout(timer);
-        timer = setTimeout(Drupal.drimage.init, 100);
+        timer = setTimeout(Drupal.drimage_improved.init, 100);
       });
 
     }
